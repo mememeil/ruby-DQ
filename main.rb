6134b03e69efc5444f45790840286f72f3b12550
mememeil <kodhikodhi@gmail.com>
2019-11-27 22:28:31 +0900

    main.rb モンスタークラスを定義してモンスターインスタンスを作成

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+# 勇者クラスの定義
 class Brave
   # attr_readerでゲッターの置換
   attr_reader :name
@@ -17,15 +18,42 @@ class Brave
   end
 end
 
+# モンスタークラスの定義
+class Monster
+  # ゲッター、セッター
+  attr_reader :name
+  attr_accessor :hp
+  attr_reader :offense
+  attr_reader :defense
+
+  # initializeメソッド 引数はhashのみの受け取り
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
+
+
 #キーワード引数（hash）でinitializeメソッドに値を渡す
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "もじゃもじゃ", hp: 1000, offense: 200, defense: 200)
 
-# ヒアドキュメント
+# ヒアドキュメント:勇者
 puts <<~TEXT
 NAME:#{brave.name}
 HP:#{brave.hp}
 OFFENSE:#{brave.offense}
 DEFENSE:#{brave.defense}
+TEXT
+
+# ヒアドキュメント:モンスター
+puts <<~TEXT
+NAME:#{monster.name}
+HP:#{monster.hp}
+OFFENSE:#{monster.offense}
+DEFENSE:#{monster.defense}
 TEXT
 
 # ダメージでHPが減る処理
