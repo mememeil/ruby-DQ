606d3634be8a6ea83915012e202cd9b48e195fab
mememeil <kodhikodhi@gmail.com>
2019-11-27 22:11:41 +0900

    main.rb 不要なコードを削除して可視性をアップ

--- main.rb
+++ main.rb
@@ -1,11 +1,8 @@
 class Brave
   # attr_readerでゲッターの置換
   attr_reader :name
-  # attr_reader :hp
   attr_reader :offense
   attr_reader :defense
-
-  # attr_writer :hp
 
   # attr_accessorで:hpのゲッターとセッターをまとめる
   # 残りのステータスは書き換え不要なのでゲッターのみでOK
@@ -18,62 +15,10 @@ class Brave
     @offense = params[:offense]
     @defense = params[:defense]
   end
-
-  # nameのセッター
-  # def name=(name)
-  #   @name = name
-  # end
-
-  # nameのゲッター
-  # def name
-  #   @name
-  # end
-
-  # hpのセッター復活
-  # def hp=(hp)
-  #   @hp = hp
-  # end
-
-  # hpのゲッター
-  # def hp
-  #   @hp
-  # end
-
-  # offenseのセッター
-  # def offense=(offense)
-  #   @offense = offense
-  # end
-
-  # offenseのゲッター
-  # def offense
-  #   @offense
-  # end
-
-  # defenseのセッター
-  # def defense=(defense)
-  #   @defense = defense
-  # end
-
-  # defenseのゲッター
-  # def defense
-  #   @defense
-  # end
-
 end
 
 #キーワード引数（hash）でinitializeメソッドに値を渡す
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-
-# brave.name = "テリー"
-# brave.hp = 500
-# brave.offense = 150
-# brave.defense = 100
-
-# 値を取得する
-# puts "NAME:#{brave.name}" 
-# puts "HP:#{brave.hp}"
-# puts "OFFENSE:#{brave.offense}"
-# puts "DEFENSE:#{brave.defense}"
 
 # ヒアドキュメント
 puts <<~TEXT
