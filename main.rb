8efcb83bb23267d5ac92b10f9fba11a03d899a20
mememeil <kodhikodhi@gmail.com>
2019-11-29 23:26:46 +0900

    main.rb モンスタークラスに変身機能を実装

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
+# 勇者クラスの定義
 class Brave
   attr_reader :name
   attr_reader :defense
   attr_reader :offense
   attr_accessor :hp
   
-   
+  # 勇者クラスの初期化 **paramsはハッシュのみ受け取り可能の設定  
   def initialize(**params)
     @name = params[:name]
     @defense = params[:defense]
@@ -12,11 +13,14 @@ class Brave
     @hp = params[:hp]
   end
   
+  # 攻撃メソッド
   def attack(monster)
     puts "#{@name}の攻撃"
-    
+
+    # 会心の一撃用の乱数    
     random = rand(4)
     
+    # 会心の一撃の場合はoffenseを1.2倍にする    
     if random == 0
       puts "会心の一撃！"
       calc_attack = @offense * 1.2 - monster.defense
@@ -25,6 +29,7 @@ class Brave
       calc_attack = @offense - monster.defense
     end
     
+    # 実際のダメージ計算 defense値とoffense値で重みを算出し、敵HPから割合でダメージ値を出す
     damage =
       case calc_attack
       when 200..1000
@@ -46,17 +51,20 @@ class Brave
       else
         monster.hp * rand(0..3) / 100
       end
+
+    monster.hp -= damage
       
-      puts "random:#{random}"
-      puts "calc_attack:#{calc_attack}"
-      puts "damage:#{damage}"
-      puts "#{monster.name}に#{damage}のダメージをあたえた！"
-      puts "#{monster.name}のHPは#{monster.hp - damage}になった"
+    # puts "random:#{random}"
+    # puts "calc_attack:#{calc_attack}"
+    # puts "damage:#{damage}"
+    puts "#{monster.name}に#{damage}のダメージをあたえた！"
+    puts "#{monster.name}のHPは#{monster.hp}になった"
   end
 end
 
+#モンスタークラスの定義
 class Monster
-  attr_reader :name
+  attr_accessor :name
   attr_reader :defense
   attr_reader :offense
   attr_accessor :hp
@@ -66,9 +74,19 @@ class Monster
     @defense = params[:defense]
     @offense = params[:offense]
     @hp = params[:hp]
+
+    @is_angry_flag = false
+    @half_hp = params[:hp] * 0.5
   end
   
   def attack(brave)
+    # モンスターが怒り状態かどうか判定:HPが半分以下になると発動
+    if @hp <= @half_hp && @is_angry_flag == false
+      @is_angry_flag = true
+      #change_classメソッドでモンスターが強化される
+      change_class
+    end
+
     puts "#{@name}の攻撃"
     
     random = rand(4)
@@ -102,13 +120,26 @@ class Monster
       else
         brave.hp * rand(0..3) / 100
       end
+
+    brave.hp -= damage
       
-      puts "random:#{random}"
-      puts "calc_attack:#{calc_attack}"
-      puts "damage:#{damage}"
-      puts "#{brave.name}に#{damage}のダメージをあたえた！"
-      puts "#{brave.name}のHPは#{brave.hp - damage}になった"
+    # puts "random:#{random}"
+    # puts "calc_attack:#{calc_attack}"
+    # puts "damage:#{damage}"
+    puts "#{brave.name}に#{damage}のダメージをあたえた！"
+    puts "#{brave.name}のHPは#{brave.hp}になった"
   end
+
+  private
+  
+    def change_class
+      puts "#{@name}は怒っている"
+      puts "#{@name}は#{@name}キングに変身した！"
+
+      @offense *= 1.2
+      @name = "#{@name}キング"
+    end
+
 end
 
 brave = Brave.new(name: "勇者", defense: 200, offense: 200, hp: 500)
