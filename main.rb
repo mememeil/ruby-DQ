9b155be3659f2c9ba29569d41d10d66ad2ec6839
mememeil <kodhikodhi@gmail.com>
2019-11-30 22:09:48 +0900

    main.rb 勇者クラスのattackメソッドをリファクタリング

--- main.rb
+++ main.rb
@@ -17,49 +17,66 @@ class Brave
   def attack(monster)
     puts "#{@name}の攻撃"
 
-    # 会心の一撃用の乱数    
-    random = rand(4)
-    
-    # 会心の一撃の場合はoffenseを1.2倍にする    
-    if random == 0
-      puts "会心の一撃！"
-      calc_attack = @offense * 1.2 - monster.defense
-    else
-      puts "通常攻撃"
-      calc_attack = @offense - monster.defense
-    end
-    
-    # 実際のダメージ計算 defense値とoffense値で重みを算出し、敵HPから割合でダメージ値を出す
-    damage =
-      case calc_attack
-      when 200..1000
-        monster.hp * rand(91..100) / 100
-      when 160...200
-        monster.hp * rand(71..90) / 100
-      when 120...160
-        monster.hp * rand(51..70) / 100
-      when 80...120
-        monster.hp * rand(31..50) / 100
-      when 40...80
-        monster.hp * rand(21..30) / 100
-      when 20...40
-        monster.hp * rand(11..20) / 100
-      when -20...20
-        monster.hp * rand(5..10) / 100
-      when -60...-20
-        monster.hp * rand(1..4) / 100
-      else
-        monster.hp * rand(0..3) / 100
-      end
+    attack_type = decision_attack_type
 
-    monster.hp -= damage
+    damage = calc_damage(monster, attack_type)
+
+    cause_damage(monster, damage)
+    # monster.hp -= damage
       
-    # puts "random:#{random}"
-    # puts "calc_attack:#{calc_attack}"
-    # puts "damage:#{damage}"
-    puts "#{monster.name}に#{damage}のダメージをあたえた！"
+    # puts "#{monster.name}に#{damage}のダメージをあたえた！"
     puts "#{monster.name}のHPは#{monster.hp}になった"
   end
+
+  private
+
+    def decision_attack_type
+      # 会心の一撃用の乱数    
+      random = rand(4)
+      if random == 0
+        "sp_attack"
+      else
+        "normal_attack"
+      end
+    end
+
+    def calc_damage(monster, attack_type)
+      if attack_type == "sp_attack"
+        puts "会心の一撃！"
+        calc_attack = @offense * 1.2 - monster.defense
+      else
+        puts "通常攻撃"
+        calc_attack = @offense - monster.defense
+      end
+      
+      # 実際のダメージ計算 defense値とoffense値で重みを算出し、敵HPから割合でダメージ値を出す
+        case calc_attack
+        when 200..1000
+          monster.hp * rand(91..100) / 100
+        when 160...200
+          monster.hp * rand(71..90) / 100
+        when 120...160
+          monster.hp * rand(51..70) / 100
+        when 80...120
+          monster.hp * rand(31..50) / 100
+        when 40...80
+          monster.hp * rand(21..30) / 100
+        when 20...40
+          monster.hp * rand(11..20) / 100
+        when -20...20
+          monster.hp * rand(5..10) / 100
+        when -60...-20
+          monster.hp * rand(1..4) / 100
+        else
+          monster.hp * rand(0..3) / 100
+        end
+    end
+
+    def cause_damage(monster, damage)
+      monster.hp -= damage
+      puts "#{monster.name}に#{damage}のダメージをあたえた！"
+    end
+
 end
 
 #モンスタークラスの定義
