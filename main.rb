cdd61e160262f3fde622a4197a52dd44a2100cd3
mememeil <kodhikodhi@gmail.com>
2019-11-30 22:39:45 +0900

    main.rb ループ処理を実装

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ class Brave
   attr_reader :defense
   attr_reader :offense
   attr_accessor :hp
+  attr_reader :full_hp
   
   # 勇者クラスの初期化 **paramsはハッシュのみ受け取り可能の設定  
   def initialize(**params)
@@ -11,6 +12,8 @@ class Brave
     @defense = params[:defense]
     @offense = params[:offense]
     @hp = params[:hp]
+
+    @full_hp = params[:hp]
   end
   
   # 攻撃メソッド
@@ -50,23 +53,23 @@ class Brave
       # 実際のダメージ計算 defense値とoffense値で重みを算出し、敵HPから割合でダメージ値を出す
         case calc_attack
         when 200..1000
-          monster.hp * rand(91..100) / 100
+          monster.full_hp * rand(91..100) / 100
         when 160...200
-          monster.hp * rand(71..90) / 100
+          monster.full_hp * rand(71..90) / 100
         when 120...160
-          monster.hp * rand(51..70) / 100
+          monster.full_hp * rand(51..70) / 100
         when 80...120
-          monster.hp * rand(31..50) / 100
+          monster.full_hp * rand(31..50) / 100
         when 40...80
-          monster.hp * rand(21..30) / 100
+          monster.full_hp * rand(21..30) / 100
         when 20...40
-          monster.hp * rand(11..20) / 100
+          monster.full_hp * rand(11..20) / 100
         when -20...20
-          monster.hp * rand(5..10) / 100
+          monster.full_hp * rand(5..10) / 100
         when -60...-20
-          monster.hp * rand(1..4) / 100
+          monster.full_hp * rand(1..4) / 100
         else
-          monster.hp * rand(0..3) / 100
+          monster.full_hp * rand(0..3) / 100
         end
     end
 
@@ -83,6 +86,7 @@ class Monster
   attr_reader :defense
   attr_reader :offense
   attr_accessor :hp
+  attr_reader :full_hp
   
   def initialize(**params)
     @name = params[:name]
@@ -92,6 +96,7 @@ class Monster
 
     @is_angry_flag = false
     @half_hp = params[:hp] * 0.5
+    @full_hp = params[:hp]
   end
   
   def attack(brave)
@@ -149,23 +154,23 @@ class Monster
       # 実際のダメージ計算 defense値とoffense値で重みを算出し、敵HPから割合でダメージ値を出す
         case calc_attack
         when 200..1000
-          brave.hp * rand(91..100) / 100
+          brave.full_hp * rand(91..100) / 100
         when 160...200
-          brave.hp * rand(71..90) / 100
+          brave.full_hp * rand(71..90) / 100
         when 120...160
-          brave.hp * rand(51..70) / 100
+          brave.full_hp * rand(51..70) / 100
         when 80...120
-          brave.hp * rand(31..50) / 100
+          brave.full_hp * rand(31..50) / 100
         when 40...80
-          brave.hp * rand(21..30) / 100
+          brave.full_hp * rand(21..30) / 100
         when 20...40
-          brave.hp * rand(11..20) / 100
+          brave.full_hp * rand(11..20) / 100
         when -20...20
-          brave.hp * rand(5..10) / 100
+          brave.full_hp * rand(5..10) / 100
         when -60...-20
-          brave.hp * rand(1..4) / 100
+          brave.full_hp * rand(1..4) / 100
         else
-          brave.hp * rand(0..3) / 100
+          brave.full_hp * rand(0..3) / 100
         end
     end
 
@@ -176,10 +181,16 @@ class Monster
 
 end
 
-brave = Brave.new(name: "勇者", defense: 200, offense: 400, hp: 500)
+brave = Brave.new(name: "勇者", defense: 200, offense: 200, hp: 500)
 monster = Monster.new(name: "もじゃもじゃ", defense: 220, offense: 230, hp: 700)
 
-
-brave.attack(monster)
-puts "------------------------------------"
-monster.attack(brave)
+loop do
+  if brave.hp <= 0 || monster.hp <= 0
+    break
+  else
+    brave.attack(monster)
+    puts "------------------------------------"
+    monster.attack(brave)
+    puts "------------------------------------"
+  end
+end
